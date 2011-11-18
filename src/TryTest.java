323fa2232e31235db104bf54bd0b35f2fccec2fa
pyang <paul_b_yang@dell.com>
2011-11-18 11:48:02 -0800

    second commit

---
+++ src/TryTest.java
@@ -0,0 +1,13 @@
+public class TryTest
+{
+   public static void main(String [] args) throws Exception
+   {
+	   String a = "a's dfdd[2]asdfad{123avc}";
+	   System.out.println(a.replaceAll("'", "''"));
+	   
+	   a = "@{ASCII:233}";
+	   String s[] = a.split("@");
+	 
+	   System.out.println(s[1].substring(s[1].indexOf(":")+1, s[1].indexOf("}")));
+	}
+}
